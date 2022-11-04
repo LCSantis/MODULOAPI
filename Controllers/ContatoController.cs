27619e13e09d7c144ff02d9556f0d1df499d6ebb
LCSantis <lucas.santis@outlook.com.br>
2022-11-04 20:28:12 -0300

    Atualização Create

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -23,7 +23,7 @@ namespace MODULOAPI.Controllers
         {
             _context.Add(contato);
             _context.SaveChanges();
-            return Ok(contato);
+            return CreatedAtAction(nameof(ObterPorId), new { id = contato.Id }, contato);
         }
 
         [HttpGet("{id}")]
